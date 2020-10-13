2c5be755edf55d101d6868aa830199c35d7414c4
nyong <nyongja@naver.com>
2020-10-13 15:21:15 +0900

    단어 변환

--- BFS_DFS/Python/word_conversion.py
+++ BFS_DFS/Python/word_conversion.py
@@ -4,12 +4,12 @@ import copy
 def check_words(word, target) :
     if (word[:-1] == target[:-1]) or (word[1:] == target[1:]):
         return True
-    if (word[0] == target[0] and word[-1] == target[-1]) :
-        for i in range(1, len(target)-1):
-            if word[:i-1] == target[:i-1] and word[i] != target[i] and word[i+1:] == target[i+1:]:
-                return True
+    cnt = 0
+    for i in range(len(word)) :
+        if word[i] == target[i] :
+            cnt += 1
+    if cnt == len(word) -1 : return True
     return False
-
 
 def solution(begin, target, words):
     answer = 0
@@ -21,17 +21,16 @@ def solution(begin, target, words):
 
     if target not in words :
         return 0
-
     while queue :
         v = queue.popleft()
-        v_new = copy.deepcopy(v)
-
+        p_word = words[v.index(max(v))]
         for index, value in enumerate(words) :
-            p_word = words[v.index(max(v))]
             if (v[index] == 0) and check_words(p_word, value):
                 if value == target :
-                    return max(v)
+                    answer = max(v)
+                    return answer
                 else :
+                    v_new = copy.deepcopy(v)
                     v_new[index] = max(v) + 1
                     queue.append(v_new)
     return answer
