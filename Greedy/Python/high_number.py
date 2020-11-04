fd6bd361c0688c5deaa4254c0459d36a99a51464
nyongja <nyongja@naver.com>
2020-11-04 15:18:56 +0900

    큰 수 만들기

--- Greedy/Python/high_number.py
+++ Greedy/Python/high_number.py
@@ -1,23 +1,15 @@
 def solution(number, k):
     answer = ''
-    num_list = list(number)
-    count = len(num_list) - k
-    start = 0
-    end = -(count)+1
-
-    while count > 0 :
-        if end == 0 :
-            if start == len(num_list) - 1 :
-                answer += num_list[-1]
-                break
-            else :
-                answer += max(num_list[start:])
-                start = start + num_list[start:].index(max(num_list[start:])) + 1
-        else :
-            answer += max(num_list[start : end])
-            start = start + num_list[start:end].index(max(num_list[start : end])) + 1
-        count -= 1
-        end = -(count)+1
+    stack = [number[0]]
+    for num in number[1:] :
+        while len(stack) > 0 and stack[-1] < num and k > 0 :
+            stack.pop()
+            k -= 1
+        stack.append(num)
+    if k != 0 :
+        stack = stack[:-k]
+    
+    answer = answer.join(stack)
     return answer
 
 
