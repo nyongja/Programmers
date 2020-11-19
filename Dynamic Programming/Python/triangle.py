13f514b4de41b721a5551ba70715319b705686f1
nyongja <nyongja@naver.com>
2020-11-19 16:54:09 +0900

    정수 삼각형

--- Dynamic Programming/Python/triangle.py
+++ Dynamic Programming/Python/triangle.py
@@ -1,16 +1,17 @@
 def solution(triangle):
     answer = 0
 
-    for row in range(len(triangle)) :
-        for value in range(len(triangle[row])):
-            if value == 0 :
-                triangle[row][value] = triangle[row][value] + triangle[row-1][value]
-            elif row == value :
-                triangle[row][value] = triangle[row][value] + triangle[row-1][value-1]
-            else :
-                triangle[row][value] = max(triangle[row-1][value-1], triangle[row-1][value] + triangle[row][value])
+    for i in range(1, len(triangle)) :
+        triangle[i][0] = triangle[i-1][0] + triangle[i][0]
+        triangle[i][-1] = triangle[i-1][-1] + triangle[i][-1]
+        for j in range(1, len(triangle[i])-1) :
+            triangle[i][j] = max(triangle[i][j] + triangle[i-1][j-1], triangle[i][j] + triangle[i-1][j])
+
     answer = max(triangle[-1])
+
     return answer
+
+
 if __name__ == "__main__" :
     triangle = []
     height = int(input())
