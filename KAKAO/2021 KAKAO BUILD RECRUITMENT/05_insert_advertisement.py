e4db8f09a0904889eaaf54791125344a4e296055
Jayun Yong <nyongja@naver.com>
2022-03-23 22:42:26 +0900

    광고 삽입

---
+++ KAKAO/2021 KAKAO BUILD RECRUITMENT/05_insert_advertisement.py
@@ -0,0 +1,19 @@
+'''
+2021 Kakao Build Recruitment
+#5 광고 삽입
+https://programmers.co.kr/learn/courses/30/lessons/72414
+'''
+
+def solution(play_time, adv_time, logs):
+    answer = ''
+    
+    return answer
+
+play_time = input()
+adv_time = input()
+n = int(input())
+logs = []
+for _ in range(n) :
+    logs.append(input())
+
+print(solution(play_time, adv_time, logs))
