cb68682a8d493283cd4bc3737d664a1977ea54b0
Jayun Yong <nyongja@naver.com>
2022-03-25 00:07:30 +0900

    스킬트리

---
+++ Level2/스킬트리.py
@@ -0,0 +1,24 @@
+def solution(skill, skill_trees):
+    answer = 0
+    
+    # 각 스킬 레벨 정리
+    skill_dict = dict()
+    for idx, value in enumerate(skill) :
+        skill_dict[value] = idx
+    
+    for skill_tree in skill_trees :
+        skill_level = 0
+        flag = True
+        for sk in skill_tree : # 스킬 하나씩 살펴보기
+            if sk in skill_dict.keys() : # 선행 스킬이 있는 스킬이면
+                if skill_dict[sk] == skill_level : # 현재 내가 배울 수 있는 스킬이면
+                    skill_level += 1
+                else :
+                    flag = False
+                    break
+        if flag :
+            answer += 1
+      
+    return answer
+
+print(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
