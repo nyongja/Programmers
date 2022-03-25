345d8a05a42a286d9d706e3de1ce4a0406428965
Jayun Yong <nyongja@naver.com>
2022-03-25 21:00:43 +0900

    타겟 넘버

--- BFS_DFS/Python/target_number.py
+++ BFS_DFS/Python/target_number.py
@@ -1,21 +1,59 @@
 from collections import deque
 
+
+from collections import deque
+
 def solution(numbers, target):
-    answer = 0
+    def bfs(start) :
+        answer = 0
+        q = deque([])
+        q.append((numbers[start], start))
+        q.append((-numbers[start], start))
+        while q :
+            result, index = q.popleft()
+            if index == len(numbers)-1 :
+                if result == target :
+                    answer += 1
+            else :
+                index += 1
+                q.append((result + numbers[index], index))
+                q.append((result - numbers[index], index))
+        return answer
+    
+    answer = bfs(0)
 
-    queue = deque()
-    queue.append(numbers[0])
-    queue.append(-numbers[0])
-
-    for n in numbers[1:] :
-        num_list = []
-        while queue :
-            num = queue.popleft()
-            num_list.append(num + n)
-            num_list.append(num - n)
-        for _ in range(len(num_list)) : queue.append(num_list.pop())
-    answer = queue.count(target)
+    
+    def dfs(result, idx) :
+        answer = 0
+        if idx == len(numbers)-1 :
+            if result == target :
+                answer += 1
+        else :
+            idx += 1
+            answer += dfs(result+numbers[idx], idx)
+            answer += dfs(result-numbers[idx], idx)
+        return answer
+    
+    #answer = dfs(0, -1)
+    
     return answer
+    
+# def solution(numbers, target):
+#     answer = 0
+
+#     queue = deque()
+#     queue.append(numbers[0])
+#     queue.append(-numbers[0])
+
+#     for n in numbers[1:] :
+#         num_list = []
+#         while queue :
+#             num = queue.popleft()
+#             num_list.append(num + n)
+#             num_list.append(num - n)
+#         for _ in range(len(num_list)) : queue.append(num_list.pop())
+#     answer = queue.count(target)
+#     return answer
 
 if __name__ == "__main__" :
     numbers = list(map(int, input().split()))
